abf902bc309722a92945848db665d4064805518c
Ludi <osrsdaenerys@gmail.com>
2024-09-26 01:15:58 +0200

    fixed active link issue

--- scripts/modules/header.js
+++ scripts/modules/header.js
@@ -7,12 +7,16 @@ export async function loadHeader() {
    * Active Link logic, add css modifier based on window.pathname
    */
   const navLinks = document.querySelectorAll("nav a");
+  const indexLink = document.getElementById("index-link");
   const currentPage = window.location.pathname;
   console.log(currentPage);
 
   navLinks.forEach((link) => {
     if (link.href.includes(`${currentPage}`)) {
       link.classList.add("active");
+      indexLink.classList.remove("active");
+    } else if (link.href.includes("")) {
+      indexLink.classList.add("active");
     }
   });
 
